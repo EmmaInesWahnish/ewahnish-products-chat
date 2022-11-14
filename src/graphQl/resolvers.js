ab1fd28c9f6a6c96fbe18d5845ccb76d75e986f8
Emma Wahnish <ewahnish@gmail.com>
2022-11-13 22:26:54 -0300

    Alta de producto

--- src/graphQl/resolvers.js
+++ src/graphQl/resolvers.js
@@ -6,15 +6,51 @@ import {
     modifyProductById,
     deleteProductById   
 } 
-from '../services/ProductService.js'
+from '../services/ProductService.js';
+
+import {
+    getAllCarts,
+    getCartsById,
+    saveCarts,
+    saveCartsArray,
+    modifyCartById,
+    deleteCartById,
+    deleteProdInCart
+}
+from '../services/CartService.js'
+
+import {
+    getAllOrders,
+    getOrdersById,
+    saveOrders,
+    saveOrdersArray,
+    modifyOrderById,
+    deleteOrderById,
+    deleteProdInOrder
+}
+from '../services/OrderService.js'
+
 const resolvers = {
     Query:{
         helloWorld: () => 'Hola mundo :)',
         getAllProducts: async() => {
             let products = await getAllProducts();
             return products;
+        },
+        getAllOrders: async()=> {
+            let orders = await getAllOrders();
+            return orders;
         }
-    }
+    },
+
+    Mutation: {
+        createProduct: async (_,newProduct) => {
+            let Response = await saveProducts(newProduct.producto);
+            return Response;
+        }
+
+   }
+
 }
 
 export default resolvers;
--- src/graphQl/typeDefs.js
+++ src/graphQl/typeDefs.js
@@ -2,7 +2,7 @@ import { gql } from 'apollo-server-express';
 
 const typeDefs = gql`
 
-    type Product {
+    type Prodord {
         id:ID
         timestamp: String
         nombre: String
@@ -11,13 +11,60 @@ const typeDefs = gql`
         foto: String
         precio: Int
         Stock: Int
+        cantidad: Int
+    }
+
+    type Product {
+        id:ID
+        timestamp: String
+        nombre: String
+        descripcion: String
+        codigo: String
+        foto: String
+        precio: Int
+        stock: Int
+    }
+
+    input ProductData {
+        timestamp: String
+        nombre: String
+        descripcion: String
+        codigo: String
+        foto: String
+        precio: Int
+        stock: Int
+    }
+
+    type Cart {
+        id: ID
+        timestamp: String
+        user_id: ID
+        productos: [Prodord]
+    }
+
+    type Order {
+        id: ID
+        timestamp: String
+        user_id: ID
+        productos:[Prodord]
     }
 
     type Query {
         helloWorld:String
         getAllProducts: [Product]
+        getAllOrders: [Order]
     }
 
+    type Response {
+        message: String
+        product: Product
+        theProductId: ID
+        whichDb: String
+    }
+
+    type Mutation {
+        createProduct(producto: ProductData): Response
+   }
 `
 
 export default typeDefs;
